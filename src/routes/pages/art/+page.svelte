49fb11005a9708a284d3d2defc5d7b73a7992987
Ben Cipa <benjamincipa@hotmail.com>
2023-04-08 15:29:18 -0400

    room looking good

--- src/routes/pages/art/+page.svelte
+++ src/routes/pages/art/+page.svelte
@@ -1,64 +1,60 @@
 <script>
-	import { Canvas, InteractiveObject, OrbitControls, T } from '@threlte/core'
-	import { spring } from 'svelte/motion'
-    import { DoubleSide } from 'three';
-    import { degToRad } from 'three/src/math/MathUtils'
-    // import { HTML } from '@threlte/extras'
+	// @ts-ignore
+    import { Canvas, T } from '@threlte/core'
+    import { DoubleSide, RepeatWrapping } from 'three';
+    import { HTML, OrbitControls, Grid } from '@threlte/extras'
+
+    export let roomSize = 10;
+    
+    $: roomOffset = roomSize / -2;
 </script>
 
-<div class="h-full w-full">
-	<Canvas linear flat>
-        <T.PerspectiveCamera makeDefault position={[10, 10, 10]} fov={24}>
-			<OrbitControls maxPolarAngle={degToRad(80)} enableZoom={false} target={{ y: 0.5 }} />
-		</T.PerspectiveCamera>
+<Canvas >
+    <T.PerspectiveCamera
+            makeDefault
+            position={[roomSize / 2, roomSize / 2, roomSize / 2]}
+        >
+        <OrbitControls />
+    </T.PerspectiveCamera>
 
-		<T.AmbientLight position={{ y: 1, z: 1 }} />
+    <T.DirectionalLight position={[3, 10, 7]} />
+    
+    <Grid />
 
-		<!-- Cube -->
-		<T.Group scale={1} >
-            <T.Mesh rotation.y={Math.PI / 2} position.x={-0.5} position.z={0.5} let:ref>
-                <T.PlaneGeometry/>
-                <T.MeshLambertMaterial
-						side={DoubleSide}
-                        args={[
-							{
-								color: 0x3f7b9d,
-							}
-						]}
-					/>
-            </T.Mesh>
-            <T.Mesh let:ref>
-                <T.PlaneGeometry  />
-                <!-- <HTML>
-                    <p class="text-2xl">BUY BUY BUY</p>
-                </HTML> -->
-                <T.MeshLambertMaterial
-						side={DoubleSide}
-                        args={[
-							{
-								color: 0x3f7b9d,
-							}
-						]}
-					/>
-            </T.Mesh>
-            <T.Mesh  rotation.x={-Math.PI / 2} position.y={-0.5} position.z={0.5} let:ref>
-                <T.PlaneGeometry  />
-                <T.MeshLambertMaterial
-						side={DoubleSide}
-                        args={[
-							{
-								color: 0x3f7b9d,
-							}
-						]}
-					/>
-            </T.Mesh>
-		</T.Group>
-	</Canvas>
-</div>
-
-<style>
-	div {
-		height: 100%;
-		width: 100%;
-	}
-</style>
+    <!-- Cube -->
+    <T.Group scale={roomSize} position={[0, 0.5, roomOffset]} >
+        <T.Mesh rotation.y={Math.PI / 2} position.x={-0.5} position.z={0.5} let:ref>
+            <T.PlaneGeometry/>
+            <T.MeshLambertMaterial
+                    side={DoubleSide}
+                    args={[
+                        {
+                            color: 0x3f7b9d,
+                        }
+                    ]}
+                />
+        </T.Mesh>
+        <T.Mesh let:ref>
+            <T.PlaneGeometry  />
+            <T.MeshLambertMaterial
+                    side={DoubleSide}
+                    args={[
+                        {
+                            color: 0x3f7b9d,
+                        }
+                    ]}
+                />
+        </T.Mesh>
+        <T.Mesh  rotation.x={-Math.PI / 2} position.y={-0.5} position.z={0.5} let:ref>
+            <T.PlaneGeometry  />
+            <T.MeshLambertMaterial
+                    side={DoubleSide}
+                    args={[
+                        {
+                            color: 0x3f7b9d,
+                        }
+                    ]}
+                />
+        </T.Mesh>
+    </T.Group>
+</Canvas>
