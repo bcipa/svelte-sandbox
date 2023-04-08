f8bfeb922a6bf690178b6152be9227a0c71481b5
Ben Cipa <benjamincipa@hotmail.com>
2023-04-08 13:49:44 -0400

    Working room interior model

--- src/routes/pages/art/+page.svelte
+++ src/routes/pages/art/+page.svelte
@@ -1,42 +1,51 @@
 <script>
 	import { Canvas, InteractiveObject, OrbitControls, T } from '@threlte/core'
 	import { spring } from 'svelte/motion'
+  import { DoubleSide } from 'three';
 	import { degToRad } from 'three/src/math/MathUtils'
-
-	const scale = spring(1)
 </script>
 
-<div>
-	<Canvas>
-		<T.PerspectiveCamera makeDefault position={[10, 10, 10]} fov={24}>
+<div class="h-full w-full">
+	<Canvas linear flat>
+        <T.PerspectiveCamera makeDefault position={[10, 10, 10]} fov={24}>
 			<OrbitControls maxPolarAngle={degToRad(80)} enableZoom={false} target={{ y: 0.5 }} />
 		</T.PerspectiveCamera>
 
-		<T.DirectionalLight castShadow position={[3, 10, 10]} />
-		<T.DirectionalLight position={[-3, 10, -10]} intensity={0.2} />
-		<T.AmbientLight intensity={0.2} />
+		<T.AmbientLight intensity={1} />
 
 		<!-- Cube -->
-		<T.Group scale={$scale}>
-			<T.Mesh position.y={0.5} castShadow let:ref>
-				<!-- Add interaction -->
-				<InteractiveObject
-					object={ref}
-					interactive
-					on:pointerenter={() => ($scale = 2)}
-					on:pointerleave={() => ($scale = 1)}
-				/>
-
-				<T.BoxGeometry />
-				<T.MeshStandardMaterial color="#333333" />
-			</T.Mesh>
+		<T.Group scale={1} >
+            <T.Mesh rotation.y={Math.PI / 2} position.x={-0.5} position.z={0.5} let:ref>
+                <T.PlaneGeometry/>
+                <T.MeshLambertMaterial
+						args={[
+							{
+								color: 0x3f7b9d,
+							}
+						]}
+					/>
+            </T.Mesh>
+            <T.Mesh let:ref>
+                <T.PlaneGeometry  />
+                <T.MeshLambertMaterial
+						args={[
+							{
+								color: 0x3f7b9d,
+							}
+						]}
+					/>
+            </T.Mesh>
+            <T.Mesh  rotation.x={-Math.PI / 2} position.y={-0.5} position.z={0.5} let:ref>
+                <T.PlaneGeometry  />
+                <T.MeshLambertMaterial
+						args={[
+							{
+								color: 0x3f7b9d,
+							}
+						]}
+					/>
+            </T.Mesh>
 		</T.Group>
-
-		<!-- Floor -->
-		<T.Mesh receiveShadow rotation.x={degToRad(-90)}>
-			<T.CircleGeometry args={[3, 72]} />
-			<T.MeshStandardMaterial color="white" />
-		</T.Mesh>
 	</Canvas>
 </div>
 
